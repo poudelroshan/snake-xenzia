7971576be73db173423d527359dcfdc41548a170
poudelroshan <poudelroshan@github.com>
2020-05-02 03:40:25 +0900

    working on basic event handling

--- snake.js
+++ snake.js
@@ -1,4 +1,33 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "green";
+var button = document.querySelector("button");
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+
+// Handles canvas color changing
+var isBlack = true;
+function changeColor() {
+	if (isBlack){
+		ctx.fillStyle = "purple";
+	}
+	else {
+		ctx.fillStyle = "black";
+	}
+	isBlack = !isBlack;
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+// When user clicks play, change 'play' button text to 'restart'
+function changeText(){
+	if (button.textContent == "Play"){
+		button.textContent = "Restart"
+	}
+}
+
+// Handles button click
+function buttonFunc(){
+	changeColor();
+	changeText();
+}
+
+button.addEventListener("click", buttonFunc);
