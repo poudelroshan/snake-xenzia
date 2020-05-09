90fc3dc61f71f012f98867905d918c6739e2ca44
poudelroshan <poudelroshan@github.com>
2020-05-09 22:17:44 +0900

    changed from black background to a pictured ground background

--- snake.js
+++ snake.js
@@ -1,25 +1,44 @@
 $(document).ready(function() {
 
+	const ground = new Image();
+	ground.src = "assets/ground.png";
+
+	const apple = new Image();
+	apple.src = "assets/food.png";
+
+	const eat = new Audio();
+	eat.src = "assets/eat.mp3";
+
+	const dead = new Audio();
+	dead.src = "assets/dead.mp3";
+
+	const keyPress = new Audio ();
+	keyPress.src = "assets/key.mp3";
+
 	var canvas = $("canvas")[0];
 	var ctx = canvas.getContext("2d");
 	var playButton = $("#play-button")[0];
 	var height = canvas.height;
 	var width = canvas.width;
-	var cw = 10; // Cell width
-	var snake = [{x:100, y:100}]; // Array for Snake
-	var direction = "RIGHT"; // Default direction of snake
+	var cw = 32; // Cell width
+	var speed = 10; // initial speed
+	var snake = [{x : Math.floor(Math.random()*17+1) * cw,
+				y : Math.floor(Math.random()*17+1) * cw}]; // Array for Snake
+	var rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 	var food;
 	var keyPressed = "RIGHT"; 
 	$(document).keydown(controller); // Add event listener for keyboard control
 	createFood();
-	setInterval(draw, 100); // Draw snake every 100 ms
+	setInterval(draw, 100)// Draw snake every 100 ms
 
 
 	function draw(){
-		
+		// clear the canvas
+		ctx.drawImage(ground, 0, 0);
+
+
 		// draw food
-		ctx.fillStyle = "yellow";
-		ctx.fillRect(food.x, food.y, cw,cw);
+		ctx.drawImage(apple, food.x, food.y);
 
 	
 
@@ -28,20 +47,24 @@ $(document).ready(function() {
 		let snakeY = snake[0].y;
 
 		// Remove the tail &/or Add a new head to simulate movement
-		snake.pop();
-
+		
 		// draw Snake
 		drawSnake();
+	
 
 		// Check the direction to change the head of snake
-		if (direction == "LEFT")	snakeX -= cw;
-		if (direction == "RIGHT")	snakeX += cw;
-		if (direction == "UP")		snakeY -= cw;
-		if (direction == "DOWN")	snakeY += cw;
+		if (keyPressed == "LEFT")	snakeX -= speed;
+		if (keyPressed == "RIGHT")	snakeX += speed;
+		if (keyPressed == "UP")		snakeY -= speed;
+		if (keyPressed == "DOWN")	snakeY += speed;
 
+		console.log("SnakeX, Snake y = " + snakeX + " " + snakeY );
+		console.log ("Food x, food y = " + food.x + " " + food.y);
 		// Check if snake eats food
 		if (snakeX == food.x && snakeY == food.y){
+			console.log("Food eaten");
 			createFood(); // Create New Food
+			eat.play();
 		}
 		else {
 			// remove the tail
@@ -53,7 +76,7 @@ $(document).ready(function() {
 			x : snakeX,
 			y : snakeY
 		}
-		snake.unshift(newHead);
+		snake.unshift(newHead); // add a new head to new position
 
 
 	}
@@ -62,8 +85,8 @@ $(document).ready(function() {
 	// Create food on the canvas
 	function createFood() {
 		food = {
-			x : Math.floor(Math.random() * (width - 30)),
-			y : Math.floor(Math.random() * (height - 30))
+			x : Math.floor(Math.random()*17+1) * 2 * cw,
+			y : Math.floor(Math.random()*17+1) * 2 * cw
 		}
 	}
 	
@@ -71,7 +94,7 @@ $(document).ready(function() {
 	// Draw the snake
 	function drawSnake(){
 		for (let i = 0; i < snake.length; i++){
-		    ctx.fillStyle = (i == 0) ? "green" : "blue"; // head is green, body is blue
+		    ctx.fillStyle = (i == 0) ? "green" : rainbow[Math.floor(Math.random() * rainbow.length)];
 			ctx.fillRect(snake[i].x, snake[i].y, cw, cw);
 		}
 	}
@@ -82,16 +105,17 @@ $(document).ready(function() {
 
 	  let key = event.which;
 
-	  if(key == "37") 			//37: LEFT
+	  if(key == "37" && keyPressed != "RIGHT") 			//37: LEFT
 		keyPressed = "LEFT";
-	  else if(key == "38") 		//38: UP
+	  else if(key == "38" && keyPressed != "DOWN") 		//38: UP
 	  	keyPressed = "UP";
-	  else if(key == "39") 		//39: RIGHT
+	  else if(key == "39" && keyPressed != "LEFT") 		//39: RIGHT
 	  	keyPressed = "RIGHT";
-	  else if(key == "40") 		//40: DOWN
+	  else if(key == "40" && keyPressed != "UP") 		//40: DOWN
 	  	keyPressed = "DOWN";
 	  else
 		console.log("User pressed invalid key");
+		keyPress.play();
 	}
 
 })
